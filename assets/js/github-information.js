9b604e6471f27b3d04e20d3ad19a13d422172c78
Arcade Kakpo <arcadej@hotmail.fr>
2024-08-23 08:40:12 +0000

    Added function to fetch gitHub information event

---
+++ assets/js/github-information.js
@@ -0,0 +1,23 @@
+function fetchGitHubInformation(event) {
+
+    var username = $("#gh-username").val();
+    if (!username) {
+        $("#gh-user-data").html(`<h2>Please enter a GitHub username</h2>`);
+        return;
+    }
+    $("#gh-user-data").html(
+        `<div id="loader">
+        <img src="assets/css/loader.gif" alt="loading..." />
+        </div>`);
+    
+    $.when(
+        $.getJSON(`https.//api.github.com/users/${username}`)
+    ).then(
+        function(response) {
+            var userData = response;
+            $("#gh-user-data").html(userInformationHTML(userData));
+        }), function(errorResponse.status === 404) {
+            $("#gh-user-data").html(
+            `<h2>No info found for user ${username}</h2> `
+            }
+}
